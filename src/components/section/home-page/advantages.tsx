9f2f60279ff172453a82c00bff04dfaa68b053e0
Dayezzz <adiasmuhsin1206@gmail.com>
2026-08-25 22:13:23 +0700

    feat(home): enhance AdvantagesSection layout and styling for better responsiveness

--- src/components/section/home-page/advantages.tsx
+++ src/components/section/home-page/advantages.tsx
@@ -13,9 +13,9 @@ export default function AdvantagesSection() {
     description: t(`listAdvantages${index + 1}.description`),
   }));
   return (
-    <section className="w-full font-sora h-[90vh] flex">
-      <div className="w-[25%] bg-third border-r-3 flex items-center h-full px-6 relative overflow-hidden">
-        <h1 className="flex flex-col items-stretch w-full text-4xl font-medium">
+    <section className="w-full font-sora lg:h-[90vh] h-fit flex lg:flex-row flex-col">
+      <div className="lg:w-[25%] w-[90%] lg:mx-0 mx-auto lg:mt-0 mt-3 z-10 lg:z-0 lg:shadow-none shadow-shadow bg-third lg:border-r-3 lg:border-0 border-3 flex items-center lg:h-full h-fit px-6 lg:py-0 py-6 relative overflow-hidden">
+        <h1 className="relative z-10 flex flex-col items-stretch w-full px-8 text-3xl font-medium lg:text-4xl lg:px-0 lg:static">
           <span className="self-start font-archivo">{t("title")} </span>
           <span className="flex items-center self-end font-bold tracking-tight uppercase">
             <HugeiconsIcon
@@ -29,7 +29,7 @@ export default function AdvantagesSection() {
         </h1>
         <StarElement />
       </div>
-      <div className="w-[55%] relative border-r-3 flex items-center justify-center h-full px-4">
+      <div className="lg:w-[55%] w-full relative lg:border-r-3 border-r-0 lg:border-b-0 border-b-3 flex items-center justify-center lg:h-full h-fit px-4 lg:py-0 py-6">
         <div className="absolute inset-0 z-0 w-full h-full opacity-50">
           <div className="relative flex w-full h-full">
             <div className="absolute inset-0 z-0 flex flex-col w-full h-full justify-evenly">
@@ -48,27 +48,29 @@ export default function AdvantagesSection() {
           {advantages.map((advantage, i) => (
             <li
               key={i}
-              className="flex flex-col justify-center items-center w-[85%] h-[20%] bg-white shadow-shadow hover:shadow-none duration-150 group border-2 relative"
+              className="flex flex-col justify-center items-center lg:w-[85%] w-[90%] lg:h-[20%] h-[18vh] bg-white shadow-shadow hover:shadow-none duration-150 group border-2 relative"
             >
-              <div className="absolute flex group-hover:shadow-none duration-150 items-center justify-center w-10 h-10 font-bold border-2 top-4 bg-main -left-6 shadow-shadow">
+              <div className="absolute flex items-center justify-center w-10 h-10 font-bold duration-150 border-2 group-hover:shadow-none top-4 bg-main -left-6 shadow-shadow">
                 {i + 1}
               </div>
-              <h2 className="text-2xl text-center px-2">{advantage.title}</h2>
-              <p className="text-lg font-public-sans max-w-[90%] text-center text-balance mt-3">
+              <h2 className="px-5 text-xl text-center lg:px-2 lg:text-2xl lg:text-nowrap text-balance">
+                {advantage.title}
+              </h2>
+              <p className="lg:text-lg text-md font-public-sans lg:max-w-[90%] max-w-full text-center mt-1.5 text-balance lg:mt-3">
                 {advantage.description}
               </p>
             </li>
           ))}
         </ul>
       </div>
-      <div className="w-[20%] bg-pink-400 flex flex-col justify-between h-full px-4 py-6">
-        <div className="flex flex-col w-full gap-6 h-fit ">
+      <div className="lg:w-[20%] w-full bg-pink-400 flex flex-col justify-between lg:items-start items-end lg:h-full h-fit px-4 lg:py-6 py-4">
+        <div className="flex flex-col w-full gap-6 h-fit">
           <h2 className="text-4xl font-black text-white uppercase">
             {t("cta.title")}
           </h2>
           <Link
             href={"/"}
-            className="flex hover:shadow-none duration-150 items-center uppercase px-6 text-lg font-bold border-2 h-14 w-fit bg-main shadow-shadow"
+            className="flex items-center px-6 text-lg font-bold uppercase duration-150 border-2 hover:shadow-none h-14 w-fit bg-main shadow-shadow"
           >
             {t("cta.btnCta")}
             <HugeiconsIcon
@@ -80,7 +82,7 @@ export default function AdvantagesSection() {
             />
           </Link>
         </div>
-        <div className="relative w-full h-[40%]">
+        <div className="relative lg:h-[40%] h-[30vh] w-[70%] lg:w-full lg:mt-0 mt-12">
           <Image
             src={"/home/computer.png"}
             alt="computer"
@@ -88,7 +90,7 @@ export default function AdvantagesSection() {
             fill
             draggable={false}
             onDragStart={(event) => event.preventDefault()}
-            className="pointer-events-none select-none object-contain"
+            className="object-contain pointer-events-none select-none"
           />
         </div>
       </div>
@@ -99,7 +101,7 @@ export default function AdvantagesSection() {
 function StarElement() {
   return (
     <>
-      <div className="absolute z-10 top-2 right-2 w-25 h-25 rotate-8">
+      <div className="absolute z-0 w-10 h-10 top-2 right-2 lg:w-25 lg:h-25 rotate-8">
         <Image
           src={"/home/star.png"}
           fill
@@ -107,10 +109,10 @@ function StarElement() {
           alt="star"
           draggable={false}
           onDragStart={(event) => event.preventDefault()}
-          className="pointer-events-none select-none object-contain"
+          className="object-contain pointer-events-none select-none"
         />
       </div>
-      <div className="absolute z-10 top-23 right-25 w-10 h-10 rotate-45">
+      <div className="absolute z-0 w-5 h-5 rotate-45 lg:w-10 lg:h-10 lg:top-23 lg:right-25 top-2 right-13">
         <Image
           src={"/home/star.png"}
           fill
@@ -118,10 +120,10 @@ function StarElement() {
           alt="star"
           draggable={false}
           onDragStart={(event) => event.preventDefault()}
-          className="pointer-events-none select-none object-contain"
+          className="object-contain pointer-events-none select-none"
         />
       </div>
-      <div className="absolute z-10 top-28 right-5 w-17 h-17 -rotate-12">
+      <div className="absolute z-0 lg:top-28 lg:right-5 top-8 right-16 lg:w-17 lg:h-17 w-7 h-7 -rotate-12">
         <Image
           src={"/home/star.png"}
           fill
@@ -129,10 +131,10 @@ function StarElement() {
           alt="thunder"
           draggable={false}
           onDragStart={(event) => event.preventDefault()}
-          className="pointer-events-none select-none object-contain"
+          className="object-contain pointer-events-none select-none"
         />
       </div>
-      <div className="absolute z-10 -top-30 -left-30 w-70 h-70 select-none rotate-12">
+      <div className="absolute z-0 select-none lg:-top-30 lg:-left-30 -top-13 -left-13 lg:w-70 lg:h-70 w-25 h-25 rotate-12">
         <Image
           src={"/home/star2.png"}
           fill
@@ -140,14 +142,14 @@ function StarElement() {
           alt="thunder"
           draggable={false}
           onDragStart={(event) => event.preventDefault()}
-          className="pointer-events-none select-none object-contain"
+          className="object-contain pointer-events-none select-none"
         />
       </div>
-      <div className="w-70 h-70 flex items-center -bottom-30 -left-30 justify-center absolute z-10 rotate-45">
+      <div className="absolute z-0 flex items-center justify-center w-40 h-40 rotate-45 lg:w-70 lg:h-70 lg:-bottom-30 -bottom-22 -left-22 lg:-left-30">
         <div className="w-full h-full bg-main shadow-shadow rotate-180 rounded-[50%] border-2"></div>
-        <div className="w-30 h-30 absolute bg-third rounded-[50%] border-3"></div>
+        <div className="lg:w-30 lg:h-30 w-20 h-20 absolute bg-third rounded-[50%] border-3"></div>
       </div>
-      <div className="absolute z-10 -bottom-10 right-0 w-[11vw] h-[25vh] -rotate-12">
+      <div className="absolute z-0 lg:-bottom-10 -bottom-28 lg:right-0 right-0 w-[11vw] h-[25vh] -rotate-12">
         <Image
           src={"/home/star3.png"}
           fill
@@ -155,7 +157,7 @@ function StarElement() {
           alt="thunder"
           draggable={false}
           onDragStart={(event) => event.preventDefault()}
-          className="pointer-events-none select-none object-contain"
+          className="object-contain pointer-events-none select-none"
         />
       </div>
     </>
